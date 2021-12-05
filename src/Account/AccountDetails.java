2dc33f4297bd97570f6eb166fc49f2788385c9e4
unknown <akshayHVshinde04@gmail.com>
2021-12-05 22:09:42 +0530

    DeckOfCards

--- src/Account/AccountDetails.java
+++
@@ -1,9 +0,0 @@
-package Account;
-
-public class AccountDetails {
-    public static void main(String[] args) {
-	AccountTest account = new AccountTest();
-	int debitAmount = account.debit();
-	System.out.println("Account Balance :"+ debitAmount );
-    }
-}
